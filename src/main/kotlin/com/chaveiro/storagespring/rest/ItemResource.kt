65683dfd78e5179089061b046b64ba8cb0517ca7
Alexandrehs <echo.alexandre@gmail.com>
2021-06-18 04:40:24 -0300

    Alteracao na rota do metodo updateStorageItem

--- src/main/kotlin/com/chaveiro/storagespring/rest/ItemResource.kt
+++ src/main/kotlin/com/chaveiro/storagespring/rest/ItemResource.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*
 import javax.websocket.server.PathParam
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("/items/")
 class ItemResource {
 
     @Autowired
@@ -27,8 +27,8 @@ class ItemResource {
         return ResponseEntity.status(HttpStatus.OK).body(items)
     }
 
-    @PutMapping("/update")
-    fun updateStorageItem(@PathParam(value = "id") id : String , @PathParam(value = "newStorage") newStorage: String) {
+    @PutMapping()
+    fun updateStorageItem(@PathParam(value = "id") id : String, @PathParam(value = "newStorage") newStorage: String) {
         val updatedItem = repository.findById(id)
         updatedItem.map {
             val item : ItemsEntity = it.copy(
