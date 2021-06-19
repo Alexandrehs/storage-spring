081ccb95a9374b1988a31bcdca34c646a7cc94ed
Alexandrehs <echo.alexandre@gmail.com>
2021-06-19 14:30:00 -0300

    put

--- src/main/kotlin/com/chaveiro/storagespring/rest/ItemResource.kt
+++ src/main/kotlin/com/chaveiro/storagespring/rest/ItemResource.kt
@@ -2,12 +2,10 @@ package com.chaveiro.storagespring.rest
 
 import com.chaveiro.storagespring.entities.ItemsEntity
 import com.chaveiro.storagespring.repository.ItemRepository
-import netscape.javascript.JSObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/items")
